5f830f2b8f7ce50579624254ede1ef2ef17130fd
Parker Brandt <parker.brandt101@gmail.com>
2025-06-21 14:43:06 -0500

    Added template for home page

--- src/main/java/com/lucentus/magictracker/controllers/HomeController.java
+++ src/main/java/com/lucentus/magictracker/controllers/HomeController.java
@@ -6,9 +6,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
+    /**
+     * Handles GET requests for root path "/"
+     * @return the String logical view name, "home"
+     */
     @GetMapping("/")
     public String home() {
         return "home";
     }
-    
+
 }
