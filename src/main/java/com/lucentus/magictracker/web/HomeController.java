1cb5ed95bdc39d59e7e56a7d982996286d3d0716
Parker Brandt <parker.brandt101@gmail.com>
2025-06-28 17:44:53 -0500

    Added some styling for home page

--- src/main/java/com/lucentus/magictracker/web/HomeController.java
+++ src/main/java/com/lucentus/magictracker/web/HomeController.java
@@ -2,15 +2,22 @@ package com.lucentus.magictracker.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+
+/**
+ * Controller for home page of website
+ * Handles all requests for root path "/"
+ */
 @Controller
+@RequestMapping("/")
 public class HomeController {
 
     /**
      * Handles GET requests for root path "/"
      * @return the String logical view name, "home"
      */
-    @GetMapping("/")
+    @GetMapping
     public String home() {
         return "home";
     }
